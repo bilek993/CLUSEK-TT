8bea5156fcfcd87e37022940d4a8a27d23bf1a9b
bilek993 <jbilinskipl@gmail.com>
2021-12-08 17:04:05 +0100

    Changed dark accent color.

--- lib/resources/app_colors.dart
+++ lib/resources/app_colors.dart
@@ -7,7 +7,7 @@ class AppColors {
 
   static const Color backgroundColor = Color(0xFF2B2748);
   static const Color accentColorLight = Color(0xFF0AE294);
-  static const Color accentColorDark = Color(0xFF1A162D);
+  static const Color accentColorDark = Color(0xFF221E3A);
   static const Color textColor = Colors.white;
   static const Color iconsColor = Colors.white;
   static const Color white = Colors.white;
