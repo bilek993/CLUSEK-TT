e72d459bff39a62a6762193674092034c00df7f2
bilek993 <jbilinskipl@gmail.com>
2021-12-08 13:42:32 +0100

    Renamed applications.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CLUSEK-TT',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         fontFamily: 'Roboto',
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"clusek_tt", origin, size)) {
+  if (!window.CreateAndShow(L"CLUSEK-TT", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
