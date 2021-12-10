257e9fcdc3ad71013baffe63729eb33b233077c2
bilek993 <jbilinskipl@gmail.com>
2021-12-10 15:30:32 +0100

    Designed subpage for files.

--- lib/ui/pages/files_subpage.dart
+++ lib/ui/pages/files_subpage.dart
@@ -1,10 +1,31 @@
+import 'package:clusek_tt/ui/widgets/settings_item_with_title.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class FilesSubpage extends StatelessWidget {
   const FilesSubpage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SettingsItemWithTitle(
+          description: AppLocalizations.of(context)!.inputFilePath,
+          controlWidget: const TextField(),
+        ),
+        SettingsItemWithTitle(
+          description: AppLocalizations.of(context)!.outputFilePath,
+          controlWidget: const TextField(),
+        ),
+        SettingsItemWithTitle(
+          description: AppLocalizations.of(context)!.autoOutputPath,
+          controlWidget: Checkbox(
+            value: true,
+            onChanged: (value) {},
+          ),
+        ),
+      ],
+    );
   }
 }
