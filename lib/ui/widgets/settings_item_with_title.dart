7809d53101ea1c7822421402ae5215e629d0395a
bilek993 <jbilinskipl@gmail.com>
2021-12-10 15:25:59 +0100

    Added settings item with title widget.

---
+++ lib/ui/widgets/settings_item_with_title.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SettingsItemWithTitle extends StatelessWidget {
+  final String description;
+  final Widget controlWidget;
+
+  const SettingsItemWithTitle({
+    Key? key,
+    required this.description,
+    required this.controlWidget,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 2.0, bottom: 2.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(description),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: controlWidget,
+          ),
+        ],
+      ),
+    );
+  }
+}
