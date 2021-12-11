3db0f7d26e9acf87366f698a1293ed92cbf97c2a
bilek993 <jbilinskipl@gmail.com>
2021-12-11 08:40:59 +0100

    Changed paddings.

--- lib/ui/widgets/subpage_item.dart
+++ lib/ui/widgets/subpage_item.dart
@@ -13,7 +13,7 @@ class SubpageItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(24.0),
+      padding: const EdgeInsets.fromLTRB(24.0, 16.0, 24.0, 16.0),
       child: child,
     );
   }
