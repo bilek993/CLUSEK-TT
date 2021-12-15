4d6995cdc904737639089bacfecd5b45930ee0ad
bilek993 <jbilinskipl@gmail.com>
2021-12-15 20:35:57 +0100

    Added presets UI.

--- lib/ui/pages/presets_subpage.dart
+++ lib/ui/pages/presets_subpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class PresetsSubpage extends StatefulWidget {
   const PresetsSubpage({Key? key}) : super(key: key);
@@ -18,12 +19,36 @@ class _PresetsSubpageState extends State<PresetsSubpage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    Size size = MediaQuery.of(context).size;
+
+    return GridView.count(
+      crossAxisCount: isSmallSize(size) ? 1 : 2,
+      childAspectRatio: isSmallSize(size)
+          ? size.width / 110
+          : size.width / (size.height / 1.4),
+      crossAxisSpacing: 14.0,
+      mainAxisSpacing: 14.0,
       controller: _scrollController,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const [],
-      ),
+      children: [
+        OutlinedButton(
+          onPressed: () {},
+          child: Text(AppLocalizations.of(context)!.albedoPresetName),
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: Text(AppLocalizations.of(context)!.emissivePresetName),
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: Text(AppLocalizations.of(context)!.combinedDataPresetName),
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: Text(AppLocalizations.of(context)!.normalDataPresetName),
+        ),
+      ],
     );
   }
+
+  bool isSmallSize(Size size) => size.width < 900.0;
 }
