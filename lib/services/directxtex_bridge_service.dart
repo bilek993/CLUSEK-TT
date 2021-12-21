e3b84d6731dee4cfeccd064919ee8d02d28e77d5
bilek993 <jbilinskipl@gmail.com>
2021-12-21 13:50:08 +0100

    Small service refactor.

--- lib/services/directxtex_bridge_service.dart
+++ lib/services/directxtex_bridge_service.dart
@@ -1,19 +1,25 @@
 import 'dart:ffi';
 
+import 'package:clusek_tt/extensions/integer_to_bool.dart';
+
 typedef CompressAndConvertToDdsFunction = Uint8 Function();
 typedef CompressAndConvertToDdsFunctionDart = int Function();
 
 class DirectxtexBridgeService {
   late final DynamicLibrary _dynamicLibrary;
-  late final CompressAndConvertToDdsFunctionDart _temperatureFunction;
+  late final CompressAndConvertToDdsFunctionDart
+      _compressAndConvertToDdsFunction;
 
   DirectxtexBridgeService() {
     _dynamicLibrary = DynamicLibrary.open('directxtex_bridge.dll');
 
-    _temperatureFunction = _dynamicLibrary.lookupFunction<
+    _compressAndConvertToDdsFunction = _dynamicLibrary.lookupFunction<
         CompressAndConvertToDdsFunction,
         CompressAndConvertToDdsFunctionDart>('compressAndConvertToDds');
   }
 
-  bool compressAndConvertToDds() => _temperatureFunction() == 1;
+  bool compressAndConvertToDds() {
+    int nativeFunctionResult = _compressAndConvertToDdsFunction();
+    return nativeFunctionResult.nativeIntegerToBool();
+  }
 }
