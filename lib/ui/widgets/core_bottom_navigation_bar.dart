f9a3f33cce41d29b657cb368469346307b892df5
bilek993 <jbilinskipl@gmail.com>
2021-12-24 22:53:56 +0100

    Removed problems counter.

--- lib/ui/widgets/core_bottom_navigation_bar.dart
+++ lib/ui/widgets/core_bottom_navigation_bar.dart
@@ -1,5 +1,4 @@
 import 'package:clusek_tt/ui/other/shadow_decoration.dart';
-import 'package:clusek_tt/ui/widgets/problems_counter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -30,7 +29,6 @@ class CoreBottomNavigationBar extends StatelessWidget {
               ),
             ),
             Expanded(child: Container(height: 1.0)),
-            const ProblemsCounter(problemsCount: 5),
             Padding(
               padding: const EdgeInsets.all(3.0),
               child: ElevatedButton(
